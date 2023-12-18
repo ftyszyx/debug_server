94814d417e57f83ca3347f0bfb97888605535eef
ftyszyx <whyzi@qq.com>
2023-12-18 18:09:52 +0800

    init

--- server/src/core/guard/jwt-auth.guard.ts
+++ server/src/core/guard/jwt-auth.guard.ts
@@ -1,4 +1,4 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from '../decorator/public.decorator';
@@ -10,12 +10,11 @@ export class JwtAuthGuard {
     private reflector: Reflector,
     private auth: AuthService,
   ) {}
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean | Promise<boolean> | Observable<boolean>> {
     const ispublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()]);
     if (ispublic) return true;
-    // console.log('jwt guard');
     const request = context.switchToHttp().getRequest() as Request;
-    const user = this.auth.CheckToken(request.headers);
+    const user = await this.auth.CheckToken(request.headers);
     request['user'] = user;
     return user != null;
   }
