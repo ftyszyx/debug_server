02c2fa630f44ddcf2d2f0c5133e70e515a5a5058
ftyszyx <whyzi@qq.com>
2024-01-15 18:43:50 +0800

    init

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,13 +6,24 @@ import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { ConfigService } from '@nestjs/config';
 import { AppHttpConfig } from './entity/config';
-
+import { Request } from 'express';
+const allowlist = [];
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'verbose'],
   });
-  
-  app.enableCors();
+  app.enableCors((req: Request, callback) => {
+    let corsOptions;
+    if (allowlist.length == 0 || allowlist.indexOf(req.header('Origin')) !== -1) {
+      // console.log("req.header('Origin')", req.header('Origin')); //如果你不需要 Cookie 可以设置为 *
+      // credentials 与前端的axios 的withCredentials（XMLHttpRequest.withCredentials）
+      // 同时 origin必须设置为访问域 才能正常访问，主要是为了 凭证是 Cookie ，授权标头或 TLS 客户端证书
+      corsOptions = { origin: req.header('Origin'), credentials: true };
+    } else {
+      corsOptions = { origin: false }; // disable CORS for this request
+    }
+    callback(null, corsOptions); // callback expects two parameters: error and options
+  });
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
